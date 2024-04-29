a5c39192d918c980589447858332cbc6bab72b30
rayen-ben-masseoud <Rayen.BEN-MASSEOUD@student.isae-supaero.fr>
2024-04-29 15:53:44 +0200

    templates

--- reservations/reservations/settings.py
+++ reservations/reservations/settings.py
@@ -38,7 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'bootstrap5'
+    'crispy_forms', 
+    'crispy_bootstrap5',
 ]
 
 MIDDLEWARE = [
