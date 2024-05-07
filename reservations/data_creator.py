1318633cc9738ae4a473428093cd791eb92e225d
Alexandre TONON <alexandre.tonon@student.isae-supaero.fr>
2024-05-07 10:11:54 +0200

    data creator etc

--- reservations/data_creator.py
+++ reservations/data_creator.py
@@ -11,8 +11,9 @@ from django.contrib.auth.models import User
 from datetime import datetime, timedelta
 from reservationhub.models import Gare, Trajet, Passager, Client, Reservation
 
-num_reservations = 500
+num_reservations = 600
 num_passengers = 0
+num_clients = 0
 
 reservations = []
 gares = Gare.objects.all()
@@ -32,9 +33,7 @@ def generate_reservations(num_reservations):
         date_reservation_random = datetime.now() + sign*timedelta(days=random.randint(1, 30))
 
         # Sélection aléatoire d'un passager et d'un client
-        client = random.choice(clients)
-        passager_du_client = Passager.objects.filter(utilisateur=client)
-        passager = random.choice(passager_du_client)
+        passager = random.choice(passagers)
 
         # Création de la réservation
         reservation = Reservation.objects.create(
@@ -43,7 +42,7 @@ def generate_reservations(num_reservations):
             numero_place=random.randint(1, 100),
             passager=passager,
             trajet=random.choice(trajets),
-            utilisateur_reservation=client
+            utilisateur_reservation=random.choice(clients),
         )
         reservations.append(reservation)
         reservation.save()
@@ -63,9 +62,23 @@ def generate_passenger(num_passengers):
         passenger.save()
     return passengers
 
+def generate_client(num_clients):
+    clients = []
+    for _ in range(num_clients):
+        client = Client.objects.create(
+            user = random.choice(users),
+            adresse =  fake.address(),
+            telephone = fake.phone_number(),
+            passager = random.choice(passagers),
+        )
+        clients.append(client)
+        client.save()
+    return clients
+
 
 # Génération de 50 réservations
 generated_reservations = generate_reservations(num_reservations)
 generated_passengers = generate_passenger(num_passengers)
+generated_clients = generate_client(num_clients)
 for passenger in generated_passengers:
     print(passenger)
